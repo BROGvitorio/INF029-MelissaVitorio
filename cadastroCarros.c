d239ce595c47905c6da7ce6b63382a88997d126d
Melissa BROG Vitorio <melissa.brog.vitorio@gmail.com>
2025-09-19 19:21:40 -0300

    corrigindo o erro + modularizando código

--- cadastroCarros.c
+++ cadastroCarros.c
@@ -9,13 +9,13 @@ void printArray(int array[MAX_CARS][QTD_INFOS])
 
     for (i = 0; i < MAX_CARS && !empty; i++)
     {
-        if (!array[i][0])
-        {
+        if (!array[0][0]) {
+            printf("Não há carros na lista!");
+        } else if (!array[i][0]) {
             printf("FIM DA LISTA DE CARROS!!!\n");
             empty = 1;
         }
-        else
-        {
+        else {
             printf("carro %d:\n", i + 1);
             for (j = 0; j < QTD_INFOS; j++)
             {
@@ -34,44 +34,43 @@ void printArray(int array[MAX_CARS][QTD_INFOS])
     }
 }
 
-int main()
-{
-    int array[MAX_CARS][QTD_INFOS] = {{1111, 2025}, {2222, 2024}};
+void addCar(int array[MAX_CARS][QTD_INFOS], int chassi, int year) {
     int i, j;
-    int emptyy = 0;
-    int chassi, year;
-
-    printArray(array);
-
-    for (i = 0; i < MAX_CARS && !emptyy; i++)
+    int empty = 0;
+    
+    for (i = 0; i < MAX_CARS && !empty; i++)
     {
-        if (!array[i][0])
+        if (!array[i+1][0])
         {
-            emptyy = 1;
+            empty = 1;
         }
-        printf("\n%d\n", i);
     }
 
-    printf("\n%d\n", i);
-    chassi = 3333;
-    year = 2023;
     for (j = 0; j < QTD_INFOS; j++)
     {
         switch (j)
         {
         case 0:
             array[i][j] = chassi;
-            printf("\n%d--%d--%d\n", i, j, array[i][j]);
             break;
 
         case 1:
             array[i][j] = year;
-            printf("\n%d--%d--%d\n", i, j, array[i][j]);
             break;
         }
     }
+}
 
-    printf("\n%d\n%d", array[2][0], array[2][1]);
+int main()
+{
+    int cars[MAX_CARS][QTD_INFOS] = {{1111, 2025}, {2222, 2024}};
+
+    printArray(cars);
+
+    addCar(cars, 3333, 2023);
+    addCar(cars, 4444, 2022);
+
+    printArray(cars);
 
     return 0;
 }
